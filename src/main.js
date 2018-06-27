d8f590d446d0c1b5ba8c4f74ebee94c7b3d229ec
riko <rikochyou@outlook.com>
2018-06-27 21:52:38 +0800

    page structure finish

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
+import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
-Vue.config.productionTip = false
-
+import 'common/stylus/index.styl'
+import fastclick from 'fastclick'
+//  Vue.config.productionTip = false
+fastclick.attach(document.body)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
-  render: h => h(App)
+  render: h => h(App),
+  router
 })
