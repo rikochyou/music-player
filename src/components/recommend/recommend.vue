f3f5090e1cd26497b6a7e3cd9f21b68a871d659f
riko <rikochyou@outlook.com>
2018-06-29 22:51:49 +0800

    lazyload

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -6,7 +6,7 @@
           <slider>
             <div v-for="(item, index) in recommends" :key="index">
               <a :href="item.linkUrl">
-                <img :src="item.picUrl"/>
+                <img class="needsclick" @load="loadImage" :src="item.picUrl"/>
               </a>
             </div>
           </slider>
@@ -16,7 +16,7 @@
           <ul>
             <li v-for="(item, index) in discList" :key=index class="item">
               <div class="icon">
-                <img width="60" height="60" :src="item.imgurl"/>
+                <img width="60" height="60" v-lazy="item.imgurl"/>
               </div>
               <div class="text">
                 <h2 class="name" v-html="item.creator.name"></h2>
@@ -47,6 +47,13 @@ export default {
     this._getDiscList()
   },
   methods: {
+    loadImage() {
+      if (!this.checkloaded) {
+        this.checkloaded = true
+        this.$refs.scroll.refresh()
+      }
+    },
+
     _getRecommend() {
       getRecommend().then((res) => {
         if (res.code === ERR_OK) {
